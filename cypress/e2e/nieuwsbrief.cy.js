4f308b1374231704674a7080d645cfc776e7ab25
AccessifySaaS <c.vandam@accessify.cloud>
2025-06-20 16:58:39 +0200

    added nieuwsbrief spec and added scheduled task

---
+++ cypress/e2e/nieuwsbrief.cy.js
@@ -0,0 +1,50 @@
+const domains = require('../fixtures/domains.js');
+
+console.log(domains)
+
+domains.forEach((domain) => {
+  if (!domain.forms.newsletter) return 
+
+  describe(`Newsletter form, structure and functionality test for ${domain.name}`, () => {
+
+  beforeEach(() => {
+    cy.visit(`https://${domain.url}/PostOffice/Form/aff8de78-01fe-47b6-9c76-abb5438c97c8`)
+  })
+
+  it('form contains visible required fields', () => {
+    cy.get('input[required]').should('have.length.at.least', 1)
+
+    cy.get('input[required]').each((el) => {
+      cy.wrap(el).should('be.visible')
+    })
+
+  });
+
+  it('form submits correctly if required fields are filled', () => {
+    cy.get('input[required]').each((el) => {
+      const type = el.attr('type')
+
+      if (type === 'email') {
+        cy.wrap(el).type('c.vandam@accessify.cloud')
+      }
+
+      if (type === 'text') {
+        cy.wrap(el).type('Cypress Test')
+      }
+
+      if (type === 'checkbox') {
+        const checkboxId = el.attr('id')
+
+        cy.get(`label[for="${checkboxId}"]`).click()
+      }
+    })
+
+    cy.get('button[type="submit"]').should('be.visible').and('not.be.disabled')
+    // todo: click submit
+  });
+});
+
+})
+
+
+
---
+++ cypress/fixtures/domains.js
@@ -0,0 +1,30 @@
+const domains = [
+  {
+    "name": "artemis",
+    "url": "artemis.accessify.cloud",
+    "forms": {
+      "newsletter": true,
+      "contact": false
+    }
+  },
+  {
+    "name": "artemis_development",
+    "url": "development.artemis.accessify.cloud",
+    "forms": {
+      "newsletter": true,
+      "contact": false
+    }
+  }
+  // ,
+  // {
+  //   "name": "hermes",
+  //   "url": "hermes.accessify.cloud",
+  //   "forms": {
+  //     "newsletter": true,
+  //     "contact": false
+  //   }
+  // }
+]
+
+module.exports = domains;
+
